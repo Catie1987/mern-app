d68e23cead37e6ac76e48853d7b2d097e688f805
Catie1987 <hieu.nguyen912@gmail.com>
2024-08-28 20:41:09 +0700

    new

--- client/src/constants/BackToTopButton.jsx
+++ client/src/constants/BackToTopButton.jsx
@@ -22,7 +22,7 @@ const scrollUp = () => {
     })
 }
   return (
-    <div className='fixed bottom-28 right-6 z-50 h-10 w-10 border-transparent'>
+    <div className='md:block hidden fixed bottom-28 right-6 z-50 h-10 w-10 border-transparent'>
         {backToTopButton && (
             <button onClick={scrollUp} className='h-full w-full flex items-center bg-slate-100 dark:bg-slate-900 
             justify-center shadow-lg border border-[--line-style2] rounded-lg text-[--deactive-text] hover:text-[--text-primary] active:text-[--cta]'>
--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,38 +1,236 @@
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import uploadToCloudinary from '../constants/upload'
+import { useState ,useCallback,useRef } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-// import required modules
-import '../style/style.css'
-import { Pagination } from 'swiper/modules'
 
-export default function CallToAction() {
+export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
+  const reactQuillRef = useRef<ReactQuill>(null);
+  const imageHandler = useCallback(() => {
+    const input = document.createElement("input");
+    
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.onchange = async () => {
+      if (input !== null && input.files !== null) {
+        const file = input.files[0];
+        const url = await uploadToCloudinary(file);
+        const quill = reactQuillRef.current;
+        if (quill) {
+          const range = quill.getEditorSelection();
+          range && quill.getEditor().insertEmbed(range.index, "image", url);
+        }
+        
+      }
+    };
+  }, []);
+
+  const handleUpdloadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Image upload failed');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError('Image upload failed');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
   return (
-    <div className=' h-50 flex flex-col sm:flex-row p-3 border-2 border-[--cta] justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
-        <div className='h-full'>
-        
-            <Swiper
-            slidesPerView={3}
-            spaceBetween={30}
-            pagination={{
-            clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-            </Swiper>
-        
-      </div>
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+          <TextInput
+            type='text'
+            placeholder='Title'
+            required
+            id='title'
+            className='flex-1'
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+          />
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
+            <option value='uncategorized'>Select a category</option>
+            <option value='travel'>Travel</option>
+            <option value='cooking'>Cooking</option>
+            <option value='place'>Place</option>
+            <option value='stay'>Stay</option>
+            <option value='beauty'>Beauty</option>
+            <option value='withlove'>With Love</option>
+            <option value='foodreview'>Food Review</option>
+            <option value='study'>Study</option>
+            <option value='movie'>Movie</option>
+            <option value='personal'>Personal</option>
+          </Select>
+        </div>
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+          <FileInput
+            type='file'
+            accept='image/*'
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button
+            type='button'
+            gradientDuoTone='purpleToBlue'
+            size='sm'
+            outline
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}
+          >
+            {imageUploadProgress ? (
+              <div className='w-16 h-16'>
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              'Upload Image'
+            )}
+          </Button>
+        </div>
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-72 object-cover'
+          />
+        )}
+        <ReactQuill
+          theme='snow'
+          placeholder='Write something...'
+          className='h-72 mb-12'
+          required
+          modules={{
+            toolbar: {
+              container: [
+                [{ header: "1" }, { header: "2" }],
+                [{ size: [] }],
+                ["bold", "italic", "underline", "strike", "blockquote"],
+                [
+                  { list: "ordered" },
+                  { list: "bullet" },
+                  { indent: "-1" },
+                  { indent: "+1" },
+                ],
+                ["link", "image", "video"],
+                
+                ["clean"],
+              ],
+              handlers: {
+                image: imageHandler,
+              },
+            },
+            clipboard: {
+              matchVisual: false,
+            },
+          }}
+          formats={[
+            "header",
+            
+            "size",
+            "bold",
+            "italic",
+            "underline",
+            "strike",
+            "blockquote",
+            "list",
+            "bullet",
+            "indent",
+            "link",
+            "image",
+            "video",
+            
+          ]}
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+            
+          }}
+        />
+        <Button type='submit' gradientDuoTone='purpleToPink'>
+          Publish
+        </Button>
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
+      </form>
     </div>
-  )
+  );
 }
-
